d1dbe24f51288e86c1e4e4a5a72b840f732b539e
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-01-29 00:22:07 +0530

    [Ajay] Display welcome to Address Book

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook obj = new AddressBook();
+            obj.display();
+            Console.ReadLine();
+        }
+    }
+
+    class AddressBook
+    {
+        public void display()
+        {
+            Console.WriteLine("Welcome to Address Book");
+        }
+    }
+}
