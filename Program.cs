c976e8105a04a2d9e6f0b2373d1018c0a0eec1cd
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-01-29 00:29:22 +0530

    [Ajay] Add create variables to contacts in the address book

--- Program.cs
+++ Program.cs
@@ -4,23 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace AddressBookSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            AddressBook obj = new AddressBook();
-            obj.display();
+            AddressBook customer1 = new AddressBook();
+            customer1.display();
             Console.ReadLine();
         }
-    }
-
-    class AddressBook
-    {
-        public void display()
+        public class AddressBook
         {
-            Console.WriteLine("Welcome to Address Book");
+            public string first_name;
+            public string last_name;
+            public string address;
+            public string city;
+            public string state;
+            public string zip;
+            public long phone_number;
+            public string email;
+
+            public void display()
+            {
+                Console.WriteLine("Welcome to Address Book");
+            }
         }
     }
 }
